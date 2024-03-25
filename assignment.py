57a148cf47397d2562be9d6194ea7b394c9bd9b2
BraydenLinsangan <159149939+BraydenLinsangan@users.noreply.github.com>
2024-03-25 13:30:03 +0000

    Added cool coders

--- assignment.py
+++ assignment.py
@@ -1,1 +1,13 @@
-
+print("Input the diameter!")
+Diameter = input()
+Diameterint = int(Diameter)
+if Diameterint <= 0:
+  print("Error Diameter is not a positive number!")
+elif Diameterint > 0:
+    Pi = 3.14
+    Radius = Diameterint / 2
+    Circumference = 2*Pi*Radius
+    Area = Pi*Radius*Radius
+    print("Results")
+    print(Circumference)
+    print(Area)
