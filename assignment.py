e49b8593e5f16bbe744d327b857e4f4f466822c9
BraydenLinsangan <159149939+BraydenLinsangan@users.noreply.github.com>
2024-03-27 13:16:06 +0000

    Added beepity boopity

--- assignment.py
+++ assignment.py
@@ -1,13 +1,23 @@
+import time
 print("Input the diameter!")
 Diameter = input()
 Diameterint = int(Diameter)
 if Diameterint <= 0:
   print("Error Diameter is not a positive number!")
 elif Diameterint > 0:
-    Pi = 3.14
-    Radius = Diameterint / 2
-    Circumference = 2*Pi*Radius
-    Area = Pi*Radius*Radius
-    print("Results")
-    print("Circumference =", Circumference)
-    print("Area =", Area)
+  Pi = 3.14
+  Radius = Diameterint / 2
+  Circumference = 2*Pi*Radius
+  Area = Pi*Radius*Radius
+  time.sleep(1)
+  print("Beep...")
+  time.sleep(1)
+  print("Boop..")
+  time.sleep(1)
+  print("Bap.")
+  time.sleep(1)
+  print("Ding!")
+  time.sleep(0.75)
+  print("Results")
+  print("Circumference =", Circumference)
+  print("Area =", Area)
